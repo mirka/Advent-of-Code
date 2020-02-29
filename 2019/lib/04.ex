31c3d6b8fbbaa717b9f3c57622df61a721fbaf78
Lena Morita <lena@jaguchi.com>
2020-02-29 09:30:22 +0900

    Solve Day 4 (Part 2) of 2019

--- 2019/lib/04.ex
+++ 2019/lib/04.ex
@@ -24,17 +24,47 @@ defmodule Day04 do
     end
   end
 
-  def is_valid(number) do
+  def has_exact_double_digit([head | tail]) do
+    cond do
+      length(tail) === 0 -> false
+      head === List.first(tail) -> has_exact_double_digit(tail, %{digit: head, count: 1})
+      true -> has_exact_double_digit(tail)
+    end
+  end
+
+  def has_exact_double_digit([head | tail], previous) when length(tail) === 0 do
+    head === previous.digit && previous.count === 1
+  end
+
+  def has_exact_double_digit([head | tail], previous) do
+    cond do
+      head !== previous.digit && previous.count === 2 ->
+        true
+
+      head === previous.digit && previous.count === 1 ->
+        has_exact_double_digit(tail, %{digit: head, count: previous.count + 1})
+
+      true ->
+        has_exact_double_digit(tail, %{digit: head, count: 1})
+    end
+  end
+
+  def is_valid1(number) do
     digits = to_int_array(number)
     has_increasing_digits(digits) && has_double_digit(digits)
   end
 
-  def solve1(first, last) do
+  def is_valid2(number) do
+    digits = to_int_array(number)
+    has_increasing_digits(digits) && has_exact_double_digit(digits)
+  end
+
+  def solve(first, last, validator) do
     Enum.reduce(first..last, 0, fn string, count ->
-      if is_valid(string), do: count + 1, else: count
+      if validator.(string), do: count + 1, else: count
     end)
   end
 end
 
-Day04.solve1(193_651, 649_729)
+Day04.solve(193_651, 649_729, &Day04.is_valid2/1)
 |> IO.inspect()
--- 2019/test/04_test.exs
+++ 2019/test/04_test.exs
@@ -2,8 +2,14 @@ defmodule Day04Test do
   use ExUnit.Case
 
   test "Part 1" do
-    assert Day04.is_valid(111_111) === true
-    assert Day04.is_valid(223_450) === false
-    assert Day04.is_valid(123_789) === false
+    assert Day04.is_valid1(111_111) === true
+    assert Day04.is_valid1(223_450) === false
+    assert Day04.is_valid1(123_789) === false
+  end
+
+  test "Part 2" do
+    assert Day04.is_valid2(112_233) === true
+    assert Day04.is_valid2(123_444) === false
+    assert Day04.is_valid2(111_122) === true
   end
 end
