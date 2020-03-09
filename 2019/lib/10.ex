eb9d875774e4e1e1038eea4a3e55640e04b6f2c4
Lena Morita <lena@jaguchi.com>
2020-03-09 09:54:00 +0900

    Solve Day 10 (Part 2) of 2019

--- 2019/lib/10.ex
+++ 2019/lib/10.ex
@@ -11,12 +11,21 @@ defmodule Day10 do
     |> elem(1)
   end
 
-  def parse_input(input) do
-    String.split(input, "\n")
-    |> Enum.reduce({0, []}, fn line, {index, result} ->
+  def convert_coordinates({x, y}, height) do
+    {x, height - 1 - y}
+  end
+
+  def parse_input(input, should_adjust_coords \\ false) do
+    lines = String.split(input, "\n")
+    height = length(lines)
+
+    Enum.reduce(lines, {0, []}, fn line, {index, result} ->
       x_coords = parse_line(line)
 
-      coords_for_line = Enum.map(x_coords, &{&1, index})
+      coords_for_line =
+        Enum.map(x_coords, fn x ->
+          if should_adjust_coords, do: convert_coordinates({x, index}, height), else: {x, index}
+        end)
 
       {index + 1, result ++ coords_for_line}
     end)
@@ -35,15 +44,18 @@ defmodule Day10 do
     end
   end
 
+  def is_virtually_equal(a, b) do
+    # floating point margin
+    error_margin = 0.001
+    a < b + error_margin && a > b - error_margin
+  end
+
   def is_collinear({:vertical, x1}, {x2, _}) do
     x1 === x2
   end
 
   def is_collinear(linear_equation, {x, y}) do
-    # floating point margin
-    error_margin = 0.0001
-    answer = linear_equation.(x)
-    answer < y + error_margin && answer > y - error_margin
+    is_virtually_equal(linear_equation.(x), y)
   end
 
   # Given asteroids on the same line, is the station sandwiched by at least two of them?
@@ -113,12 +125,114 @@ defmodule Day10 do
     {best, count}
   end
 
+  def get_angle_deg({x1, y1}, {x2, y2}) do
+    case x1 === x2 do
+      true ->
+        # vertical line
+        if y2 < y1, do: 180.0, else: 0.0
+
+      false ->
+        slope = (y2 - y1) / (x2 - x1)
+
+        deg = :math.atan(slope) * (180 / :math.pi())
+
+        cond do
+          deg === 0.0 ->
+            # horizontal line
+            if x2 < x1, do: 270.0, else: 90.0
+
+          x2 > x1 ->
+            90.0 - deg
+
+          x2 < x1 ->
+            270 - deg
+
+          true ->
+            180 + deg
+        end
+    end
+  end
+
+  def index_by_angle(station, asteroids) do
+    index_by_angle(station, asteroids, %{})
+  end
+
+  def index_by_angle(_, nil, asteroids_by_angle) do
+    asteroids_by_angle
+  end
+
+  def index_by_angle(station, [asteroid | remaining], asteroids_by_angle) do
+    angle_deg = get_angle_deg(station, asteroid)
+
+    same_angle =
+      Enum.group_by(remaining, fn ast ->
+        if station === ast do
+          :identity
+        else
+          is_virtually_equal(angle_deg, get_angle_deg(station, ast))
+        end
+      end)
+
+    sorted_asteroids =
+      Enum.sort_by(
+        Map.get(same_angle, true, []) ++ [asteroid],
+        & &1,
+        &sort_by_distance_from_station(station, &1, &2)
+      )
+
+    updated_map = Map.put(asteroids_by_angle, angle_deg, sorted_asteroids)
+
+    index_by_angle(station, Map.get(same_angle, false), updated_map)
+  end
+
+  def sort_by_distance_from_station({station_x, station_y}, {x1, y1}, {x2, y2}) do
+    [distance1, distance2] =
+      if station_x === x1 do
+        [y1 - station_y, y2 - station_y]
+      else
+        [x1 - station_x, x2 - station_x]
+      end
+
+    abs(distance1) <= abs(distance2)
+  end
+
+  def map_to_sorted_list(map) do
+    Map.keys(map)
+    |> Enum.sort()
+    |> Enum.map(fn key ->
+      Map.get(map, key)
+    end)
+  end
+
+  def asteroid_vaporized_at(asteroids, asteroid_index, current_index \\ 0)
+
+  def asteroid_vaporized_at([[first_head | _] | _], asteroid_index, current_index)
+      when asteroid_index === current_index do
+    first_head
+  end
+
+  def asteroid_vaporized_at([[_ | first_tail] | tail], asteroid_index, current_index) do
+    next_list = tail ++ if length(first_tail) > 0, do: [first_tail], else: []
+    asteroid_vaporized_at(next_list, asteroid_index, current_index + 1)
+  end
+
   def solve1(input) do
     input
     |> parse_input()
     |> get_best_location
   end
+
+  def solve2(input, station, height, asteroid_index) do
+    asteroids =
+      input
+      |> parse_input(true)
+
+    index_by_angle(station, asteroids)
+    |> map_to_sorted_list
+    |> asteroid_vaporized_at(asteroid_index - 1)
+    |> convert_coordinates(height)
+  end
 end
 
-Day10.solve1(input)
-|> IO.inspect()
+# Day10.solve2(input, Day10.convert_coordinates({23, 29}, 33), 33, 200)
+# |> IO.inspect()
--- 2019/test/10_test.exs
+++ 2019/test/10_test.exs
@@ -2,6 +2,18 @@ defmodule Day10Test do
   use ExUnit.Case
   import Day10
 
+  def test_asteroids_by_range(range, func) do
+    Enum.map(range, func)
+  end
+
+  def test_sort_by_distance(station, asteroids) do
+    Enum.sort_by(
+      asteroids,
+      & &1,
+      &sort_by_distance_from_station(station, &1, &2)
+    )
+  end
+
   test "Parse input" do
     input = ".#\n#."
     assert parse_input(input) === [{1, 0}, {0, 1}]
@@ -128,36 +140,7 @@ defmodule Day10Test do
     assert result === {{6, 3}, 41}
   end
 
-  test "Get best location 5" do
-    input = ".#..##.###...#######
-##.############..##.
-.#.######.########.#
-.###.#######.####.#.
-#####.##.#.##.###.##
-..#####..#.#########
-####################
-#.####....###.#.#.##
-##.#################
-#####.##.###..####..
-..######..##.#######
-####.##.####...##..#
-.#####..#.######.###
-##...#.##########...
-#.##########.#######
-.####.#.###.###.#.##
-....##.##.###..#####
-.#.#.###########.###
-#.#.#.#####.####.###
-###.##.####.##.#..##"
-
-    result =
-      parse_input(input)
-      |> get_best_location()
-
-    assert result === {{11, 13}, 210}
-  end
-
-  test "Temp" do
+  test "Floating point errors" do
     input = ".#..##.###...#######
 ##.############..##.
 .#.######.########.#
@@ -185,5 +168,126 @@ defmodule Day10Test do
 
   test "Don't detect self" do
     assert detectable_asteroid_count({1, 1}, [{1, 1}]) === 0
+  end
+
+  test "Get angle" do
+    assert get_angle_deg({0, 0}, {0, 1}) === 0.0
+    assert get_angle_deg({0, 0}, {1, 1}) === 45.0
+    assert get_angle_deg({0, 0}, {1, 0}) === 90.0
+    assert get_angle_deg({0, 1}, {1, 0}) === 135.0
+    assert get_angle_deg({0, 1}, {0, 0}) === 180.0
+    assert get_angle_deg({1, 1}, {0, 0}) === 225.0
+    assert get_angle_deg({1, 0}, {0, 0}) === 270.0
+    assert get_angle_deg({1, 0}, {0, 1}) === 315.0
+  end
+
+  test "Sort by distance from station" do
+    assert test_sort_by_distance({0, 1}, [{0, 2}, {0, 3}]) === [{0, 2}, {0, 3}]
+    assert test_sort_by_distance({1, 1}, [{2, 2}, {3, 3}]) === [{2, 2}, {3, 3}]
+    assert test_sort_by_distance({1, 0}, [{3, 0}, {2, 0}]) === [{2, 0}, {3, 0}]
+    assert test_sort_by_distance({1, 1}, [{4, 4}, {2, 2}]) === [{2, 2}, {4, 4}]
+    assert test_sort_by_distance({5, 5}, [{5, 4}, {5, 2}]) === [{5, 4}, {5, 2}]
+    assert test_sort_by_distance({5, 5}, [{4, 4}, {1, 1}]) === [{4, 4}, {1, 1}]
+    assert test_sort_by_distance({5, 5}, [{4, 5}, {1, 5}]) === [{4, 5}, {1, 5}]
+    assert test_sort_by_distance({5, 0}, [{4, 1}, {3, 2}]) === [{4, 1}, {3, 2}]
+  end
+
+  test "Vaporized order 1" do
+    input = ".#....#####...#..
+##...##.#####..##
+##...#...#.#####.
+..#.....#...###..
+..#.#.....#....##"
+
+    height = 5
+    station = convert_coordinates({8, 3}, height)
+    solver = &solve2(input, station, height, &1)
+
+    assert test_asteroids_by_range(1..9, solver) === [
+             {8, 1},
+             {9, 0},
+             {9, 1},
+             {10, 0},
+             {9, 2},
+             {11, 1},
+             {12, 1},
+             {11, 2},
+             {15, 1}
+           ]
+
+    assert test_asteroids_by_range(10..18, solver) === [
+             {12, 2},
+             {13, 2},
+             {14, 2},
+             {15, 2},
+             {12, 3},
+             {16, 4},
+             {15, 4},
+             {10, 4},
+             {4, 4}
+           ]
+
+    assert test_asteroids_by_range(19..27, solver) === [
+             {2, 4},
+             {2, 3},
+             {0, 2},
+             {1, 2},
+             {0, 1},
+             {1, 1},
+             {5, 2},
+             {1, 0},
+             {5, 1}
+           ]
+
+    assert test_asteroids_by_range(28..36, solver) === [
+             {6, 1},
+             {6, 0},
+             {7, 0},
+             {8, 0},
+             {10, 1},
+             {14, 0},
+             {16, 1},
+             {13, 3},
+             {14, 3}
+           ]
+  end
+
+  test "Vaporized order 2" do
+    input = ".#..##.###...#######
+##.############..##.
+.#.######.########.#
+.###.#######.####.#.
+#####.##.#.##.###.##
+..#####..#.#########
+####################
+#.####....###.#.#.##
+##.#################
+#####.##.###..####..
+..######..##.#######
+####.##.####...##..#
+.#####..#.######.###
+##...#.##########...
+#.##########.#######
+.####.#.###.###.#.##
+....##.##.###..#####
+.#.#.###########.###
+#.#.#.#####.####.###
+###.##.####.##.#..##"
+
+    height = 20
+    station = convert_coordinates({11, 13}, height)
+
+    first_three_asteroids =
+      Enum.map(1..3, fn index ->
+        solve2(input, station, height, index)
+      end)
+
+    assert first_three_asteroids === [{11, 12}, {12, 1}, {12, 2}]
+    assert solve2(input, station, height, 20) === {16, 0}
+    assert solve2(input, station, height, 50) === {16, 9}
+    assert solve2(input, station, height, 100) === {10, 16}
+    assert solve2(input, station, height, 199) === {9, 6}
+    assert solve2(input, station, height, 200) === {8, 2}
+    assert solve2(input, station, height, 299) === {11, 1}
   end
 end
