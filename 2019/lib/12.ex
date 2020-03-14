74279d1f0beb4aa7f231b5ba28afb81b41db472f
Lena Morita <lena@jaguchi.com>
2020-03-15 08:25:06 +0900

    Solve Day 12 (Part 1) of 2019

---
+++ 2019/lib/12.ex
@@ -0,0 +1,112 @@
+{:ok, input} = File.read("inputs/12.txt")
+
+defmodule Moon do
+  defstruct position: %{x: 0, y: 0, z: 0}, velocity: %{x: 0, y: 0, z: 0}
+end
+
+defmodule Day12 do
+  def parse_input(string) do
+    string
+    |> String.split("\n")
+    |> Enum.map(fn str ->
+      captures = Regex.named_captures(~r/<x=(?<x>.+?), y=(?<y>.+?), z=(?<z>.+?)>/, str)
+
+      for {key, value} <- captures,
+          into: %{},
+          do: {String.to_atom(key), elem(Integer.parse(value), 0)}
+    end)
+  end
+
+  def to_moons(positions) do
+    Enum.reduce(positions, {%{}, 0}, fn moon, {moons, index} ->
+      updated_map = Map.put(moons, index, %Moon{position: moon})
+      {updated_map, index + 1}
+    end)
+    |> elem(0)
+  end
+
+  def do_step(moons) do
+    moons
+    |> apply_gravity()
+    |> apply_velocity()
+  end
+
+  def step_for(moons, count, current \\ 1)
+
+  def step_for(moons, count, current) when current > count do
+    moons
+  end
+
+  def step_for(moons, count, current) do
+    do_step(moons)
+    |> step_for(count, current + 1)
+  end
+
+  def get_gravity_adjustment_for_point(n, other_n) do
+    cond do
+      n === other_n -> 0
+      n < other_n -> 1
+      true -> -1
+    end
+  end
+
+  def apply_gravity_for_moon(moon, other_moon) do
+    adjustments =
+      for {key, value} <- moon.position,
+          into: %{},
+          do: {key, get_gravity_adjustment_for_point(value, other_moon.position[key])}
+
+    updated_velocity =
+      for {key, value} <- moon.velocity, into: %{}, do: {key, value + adjustments[key]}
+
+    Map.put(moon, :velocity, updated_velocity)
+  end
+
+  def apply_gravity(moons, index \\ 0)
+
+  def apply_gravity(moons, index) when index > 3 do
+    moons
+  end
+
+  def apply_gravity(moons, index) do
+    [_ | tail] = Enum.to_list(index..3)
+
+    Enum.reduce(tail, moons, fn other_i, moons ->
+      Map.put(moons, index, apply_gravity_for_moon(moons[index], moons[other_i]))
+      |> Map.put(other_i, apply_gravity_for_moon(moons[other_i], moons[index]))
+    end)
+    |> apply_gravity(index + 1)
+  end
+
+  def apply_velocity(%Moon{position: position, velocity: velocity} = moon) do
+    updated_position = for {key, value} <- position, into: %{}, do: {key, value + velocity[key]}
+    Map.put(moon, :position, updated_position)
+  end
+
+  def apply_velocity(moons) do
+    for {key, value} <- moons, into: %{}, do: {key, apply_velocity(value)}
+  end
+
+  def calculate_coord_energy(coords) do
+    Enum.reduce(coords, 0, fn {_, value}, sum ->
+      sum + abs(value)
+    end)
+  end
+
+  def calculate_total_energy(moons) do
+    Enum.reduce(moons, 0, fn {_, %{position: position, velocity: velocity}}, total ->
+      total + calculate_coord_energy(position) * calculate_coord_energy(velocity)
+    end)
+  end
+
+  def solve1(input) do
+    input
+    |> parse_input()
+    |> to_moons()
+    |> step_for(1000)
+    |> calculate_total_energy()
+  end
+end
+
+# Day12.solve1(input)
+# |> IO.inspect()
---
+++ 2019/test/12_test.exs
@@ -0,0 +1,58 @@
+defmodule Day12Test do
+  use ExUnit.Case
+  import Day12
+
+  test "Parse input" do
+    input = "<x=-1, y=0, z=2>\n<x=2, y=-10, z=-7>"
+    assert parse_input(input) === [%{x: -1, y: 0, z: 2}, %{x: 2, y: -10, z: -7}]
+  end
+
+  test "Apply gravity for moon" do
+    moon = %Moon{position: %{x: -1, y: 0, z: 2}}
+    other_moon = %Moon{position: %{x: 2, y: -10, z: -7}}
+    assert apply_gravity_for_moon(moon, other_moon).velocity === %{x: 1, y: -1, z: -1}
+  end
+
+  test "Apply velocity" do
+    input = "<x=-1, y=0, z=2>"
+
+    moons =
+      parse_input(input)
+      |> to_moons()
+      |> Enum.map(fn {key, moon} ->
+        {key, Map.put(moon, :velocity, %{x: 1, y: -2, z: 3})}
+      end)
+
+    assert apply_velocity(moons)[0].position === %{x: 0, y: -2, z: 5}
+  end
+
+  test "Do step" do
+    input = "<x=-1, y=0, z=2>
+<x=2, y=-10, z=-7>
+<x=4, y=-8, z=8>
+<x=3, y=5, z=-1>"
+
+    assert parse_input(input) |> to_moons() |> step_for(1) === %{
+             0 => %Moon{position: %{x: 2, y: -1, z: 1}, velocity: %{x: 3, y: -1, z: -1}},
+             1 => %Moon{position: %{x: 3, y: -7, z: -4}, velocity: %{x: 1, y: 3, z: 3}},
+             2 => %Moon{position: %{x: 1, y: -7, z: 5}, velocity: %{x: -3, y: 1, z: -3}},
+             3 => %Moon{position: %{x: 2, y: 2, z: 0}, velocity: %{x: -1, y: -3, z: 1}}
+           }
+  end
+
+  test "Calculate coordinate energy" do
+    assert calculate_coord_energy(%{x: 2, y: 1, z: -3}) === 6
+  end
+
+  test "Calculate total energy" do
+    input = "<x=-1, y=0, z=2>
+<x=2, y=-10, z=-7>
+<x=4, y=-8, z=8>
+<x=3, y=5, z=-1>"
+
+    assert parse_input(input)
+           |> to_moons()
+           |> step_for(10)
+           |> calculate_total_energy() === 179
+  end
+end
